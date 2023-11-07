7b87773f8b3b8666934855a04eeb09b5bdcd6447
Tranhuuphuc123 <Cantho1995ct@gmail.com>
2023-11-07 11:33:20 +0700

    luu thay doi 07/11/2023

---
+++ HOC-C#/CsharpOOP/Csharp_OOP/Csharp_OOP/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello");
+
+            Console.ReadLine();
+        }
+    }
+}
